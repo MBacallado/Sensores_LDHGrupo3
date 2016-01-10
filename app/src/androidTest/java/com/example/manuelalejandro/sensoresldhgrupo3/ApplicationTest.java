464060d4422ccd45d2175f511b6450e74637409b
mamu21 <manuelbacallado89@gmail.com>
2016-01-10 21:03:30 +0000

    "LDH1-3 Codigo de las pruebas unitarias comentadas, estas pruebas se encuentran en la clase ApplicationTest"

--- app/src/androidTest/java/com/example/manuelalejandro/sensoresldhgrupo3/ApplicationTest.java
+++ app/src/androidTest/java/com/example/manuelalejandro/sensoresldhgrupo3/ApplicationTest.java
@@ -1,6 +1,8 @@
 package com.example.manuelalejandro.sensoresldhgrupo3;
 
 import android.app.Application;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.test.ApplicationTestCase;
 
 /**
@@ -8,6 +10,10 @@ import android.test.ApplicationTestCase;
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
 
+    /**
+     * Se declaran las instancias para cada uno de los activity que representaran a los sensores.
+     *
+     */
     ProximidadActivity proxi = new ProximidadActivity();
     AcelerometroActivity acel = new AcelerometroActivity();
     GiroscopioActivity giro = new GiroscopioActivity();
@@ -18,23 +24,39 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     PulsometroActivity pulso = new PulsometroActivity();
     TermometroActivity termo = new TermometroActivity();
 
-
     public ApplicationTest() {
 
         super(Application.class);
     }
 
+    /**
+     * Se usa la variable de tipo sensores para hacer las comprobaciones.Cuando se instancia un objeto, se llama al metodo onCreate, que es donde
+     * se realiza la propia comprobacion mediante la sentencia por ejemplo: sensorM.getSensorList(Sensor.TYPE_PROXIMITY) siendo sensorM de tipo
+     * sensorManager, de que el sensor esta disponible, esto varia en cada dispositivo.
+     */
 
+    /**
+     * Se crea el test para el sensor Proximidad
+     * @throws Exception
+     */
     public void testProximidad() throws Exception {
         assertNotNull("Existe Sensor", proxi.sensores != null);
 
     }
 
+    /**
+     * Se crea el test para el sensor Humedad
+     * @throws Exception
+     */
     public void testHumedad() throws Exception {
         assertNotNull("Existe Sensor", hum.sensores != null);
 
     }
 
+    /**
+     * Se crea el test para el sensor Magnometro
+     * @throws Exception
+     */
     public void testMagnometro() throws Exception {
         assertNotNull("Existe Sensor", magno.sensores != null);
 
